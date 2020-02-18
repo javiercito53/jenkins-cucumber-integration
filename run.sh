d0666ceab5e605914b6f7677a4cac7740e7c8476
Your Name <you@example.com>
2020-02-18 15:26:15 -0600

    add google chrome linux version

--- run.sh
+++ run.sh
@@ -1,15 +1,18 @@
 #wget
 #https://stackoverflow.com/questions/46109812/steps-to-install-and-run-headless-chrome-browser-on-centos-6-5-using-chrome-driv
+
+npm install pm2
+npm install cucumber
+
 mkdir Ambientes
 cd Ambientes
 mkdir Ambiente-node
 cd Ambiente-node
 
-npm install pm2
-npm install cucumber
-npm install chromeless
-
+#front
 git clone https://gitlab.com/braianflorian/react-cucumber.git
 cd react-cucumber
 yarn install
+
 pm2 start server.js
+yarn test-centos
